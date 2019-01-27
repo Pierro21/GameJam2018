332ba449dcbee7ee19a8600f03b9f3e822561ec9
lucasGras <lucas.gras@epitech.eu>
2019-01-27 15:22:21 +0100

    Final push, love global game jam

---
+++ Assets/explosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
